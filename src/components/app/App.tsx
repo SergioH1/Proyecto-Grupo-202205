2f582cd420ce1e9d7d9896c1ed9c8912e1324ab6
Marina Navarrete <mnavarretem@gmail.com>
2022-06-06 12:02:04 +0200

    Add footer and pag login

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Footer } from '../footer/Footer';
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <h1>Prision Daters</h1>
+            </div>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default App;
