e61b958a0145ff344f38a9cddcfc00fd81d84cda
Sergio H <sergiohernandezroman@gmail.com>
2022-06-07 10:34:14 +0200

    add update container-selector

--- src/components/Girls/girls.tsx
+++ src/components/Girls/girls.tsx
@@ -1,4 +1,4 @@
-export function Girl() {
+export function Girls() {
     return (
         <li key="picture-girl">
             <img src="./img/presos-girl-home.jpg" alt="" />
---
+++ src/pages/selector.tsx
@@ -0,0 +1,11 @@
+import { ContainerSelector } from '../components/containers/container-selector';
+
+export function Selector() {
+    return (
+        <>
+            <ContainerSelector></ContainerSelector>
+        </>
+    );
+}
+
+export default Selector;
