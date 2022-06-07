32e550cf9483ca7b05dae893b7530f8e81a4395a
Sergio H <sergiohernandezroman@gmail.com>
2022-06-07 17:48:25 +0200

    add home page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,8 @@
-import { InfoHome } from '../components/info-home/info-home';
-
 export function Login() {
     return (
         <>
             <h1>Prision Dates</h1>
             <img src="../public/img/Poli-inicio.jpg" alt="" />
-            <InfoHome></InfoHome>
         </>
     );
 }
