36b0453a90059d6aa398e7b54a1e2fac401e3014
Alejandro Montes <ango19941994@gmail.com>
2022-06-07 17:41:55 +0200

    Add charges component

---
+++ src/components/info-charges/info-charges.tsx
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { PrisionersContext } from '../../context/prisioner-context';
+
+export function InfoCharges() {
+    const { prisioners } = useContext(PrisionersContext);
+    const prisioner = prisioners[0];
+    return (
+        <>
+            <h3>Cargos</h3>
+            {prisioners.length
+                ? prisioner.charges.map(() => {
+                      return <p key={prisioner.id}>{prisioner.charges}</p>;
+                  })
+                : 'loading'}
+        </>
+    );
+}
--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,9 +1,13 @@
+import { InfoCharges } from '../info-charges/info-charges';
+import { InfoLocation } from '../info-location/info-location';
 import { InfoName } from '../info-name/info-name';
 
 export function Info() {
     return (
         <div className="info-container">
             <InfoName></InfoName>
+            <InfoLocation></InfoLocation>
+            <InfoCharges></InfoCharges>
         </div>
     );
 }
