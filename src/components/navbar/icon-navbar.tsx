396216f6382be2965096c88e20a4f3b196e2bf8a
Sergio H <sergiohernandezroman@gmail.com>
2022-06-07 19:27:21 +0200

    delete src button img

--- src/components/navbar/icon-navbar.tsx
+++ src/components/navbar/icon-navbar.tsx
@@ -2,7 +2,7 @@ export function IconNavBar() {
     let template = (
         <>
             <li key="heart">
-                <img role="button" src="nav__icon-heart" alt="icon heart" />
+                <img role="button" src="" alt="icon heart" />
             </li>
         </>
     );
