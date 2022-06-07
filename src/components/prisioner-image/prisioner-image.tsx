77636909cb43f5d3aa1c8d188124166baa63268b
Alejandro Montes <ango19941994@gmail.com>
2022-06-07 16:51:03 +0200

    Add image component

--- src/components/prisioner-image/prisioner-image.tsx
+++ src/components/prisioner-image/prisioner-image.tsx
@@ -5,6 +5,10 @@ export function PrisionerImage() {
     const { prisioners } = useContext(PrisionersContext);
     const prisioner = prisioners[0];
 
-    // return <img src={prisioners[0].mugshot} alt={prisioners[0].name} />;
-    return <p>{prisioners.length ? prisioner.name : 'loading'}</p>;
+    return (
+        <img
+            src={prisioners.length ? prisioner.mugshot : 'loading'}
+            alt={prisioners.length ? prisioner.name : 'loading'}
+        />
+    );
 }
