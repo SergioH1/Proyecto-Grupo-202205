dd19e20cc0f30dc8408ed3ca994ffdf69a914426
Sergio H <sergiohernandezroman@gmail.com>
2022-06-08 18:45:44 +0200

    add texting  info-home

--- src/components/info-home/info-home.test.tsx
+++ src/components/info-home/info-home.test.tsx
@@ -1,12 +1,20 @@
 import { screen, render } from '@testing-library/react';
-
+import { useContext } from 'react';
+import { PrisionersContext } from '../../context/prisioner-context';
+import { PrisionerContextProvider } from '../../context/prisioner-provider';
 import { InfoHome } from './info-home';
 
 describe('Given the component InfoHome', () => {
+    const { prisioners } = useContext(PrisionersContext);
+    const prisioner = prisioners[0];
     describe('when it is called', () => {
         test('should print', () => {
-            render(<InfoHome></InfoHome>);
-            const display = screen.getByText(/,/);
+            render(
+                <PrisionerContextProvider>
+                    <InfoHome></InfoHome>
+                </PrisionerContextProvider>
+            );
+            const display = screen.getByText(prisioner.name);
             expect(display).toBeInTheDocument();
         });
     });
