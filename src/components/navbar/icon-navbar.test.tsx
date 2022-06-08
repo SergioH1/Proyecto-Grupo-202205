705cf1074c6705b47bd0be72c6d150f875befc40
Sergio H <sergiohernandezroman@gmail.com>
2022-06-08 10:28:39 +0200

    add test icon-navbar

---
+++ src/components/navbar/icon-navbar.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { IconNavBar } from './icon-navbar';
+
+describe('Given the component Footer', () => {
+    describe('When is called', () => {
+        test('Then it sould print', () => {
+            render(<IconNavBar></IconNavBar>);
+            const display = screen.getByRole(/button/);
+            expect(display).toBeInTheDocument();
+        });
+    });
+});
