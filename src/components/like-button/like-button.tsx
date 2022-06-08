c8e87a954410daaabbc3c332aa015a9e196843c9
Alejandro Montes <ango19941994@gmail.com>
2022-06-08 14:02:39 +0200

    Add functionallity to like button

--- src/components/like-button/like-button.tsx
+++ src/components/like-button/like-button.tsx
@@ -1,6 +1,26 @@
+import { SyntheticEvent, useContext } from 'react';
+import { PrisionerLocalContext } from '../../context/local-context';
+import { PrisionersContext } from '../../context/prisioner-context';
+import { PrisionerModel } from '../../models/prisioner-local';
+
 export function LikeButton() {
+    const { addPrisioner } = useContext(PrisionerLocalContext);
+    const { prisioners } = useContext(PrisionersContext);
+    const prisioner = prisioners[0];
+
+    function handleMatch(ev: SyntheticEvent) {
+        ev.preventDefault();
+        console.log('Guardando');
+        addPrisioner(new PrisionerModel(prisioner.name, prisioner.mugshot));
+    }
+
     return (
-        <button className="like-button">
+        <button
+            className="like-button"
+            onClick={(ev) => {
+                handleMatch(ev);
+            }}
+        >
             <img src="./svg/like.svg" alt="Icon of the dislike button" />
         </button>
     );
