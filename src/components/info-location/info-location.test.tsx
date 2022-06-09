803b92daa7b018cea2c97c2535f3776c9f873121
Sergio H <sergiohernandezroman@gmail.com>
2022-06-09 16:18:14 +0200

    add infolocation test

--- src/components/info-location/info-location.test.tsx
+++ src/components/info-location/info-location.test.tsx
@@ -1,11 +1,45 @@
 import { render, screen } from '@testing-library/react';
+import { PrisionersContext } from '../../context/prisioner-context';
+import { iPrisoner } from '../../interface/prisioner-interface';
 import { InfoLocation } from './info-location';
 
 describe('Given the component InfoLocation', () => {
+    const mockPrisioners: {
+        prisioners: iPrisoner[];
+    } = {
+        prisioners: [
+            {
+                county_state: '',
+                name: 'Pepe',
+                charges: ['charges'],
+                id: 0,
+                source: 'Maricopa',
+                details: [
+                    ['s', 0],
+                    ['3', 1],
+                    ['3', 0],
+                ],
+                mugshot: ' string',
+                book_date: '',
+                source_id: '',
+                more_info_url: '',
+            },
+        ],
+    };
     describe('When is called', () => {
         test('Then it sould print', () => {
             render(<InfoLocation></InfoLocation>);
             const display = screen.getByText(/Location/i);
+            expect(display).toBeInTheDocument();
+        });
+        test('Then it sould print source', () => {
+            render(
+                <PrisionersContext.Provider value={mockPrisioners}>
+                    <InfoLocation></InfoLocation>
+                </PrisionersContext.Provider>
+            );
+
+            const display = screen.getByText(/Maricopa/i);
             expect(display).toBeInTheDocument();
         });
     });
