271f852bfdb94bcd20a9e9c6c27a33f08dd83e07
Sergio H <sergiohernandezroman@gmail.com>
2022-06-09 16:34:26 +0200

    add prisioner.test

--- src/components/info-prisioner/info-prisioner.test.tsx
+++ src/components/info-prisioner/info-prisioner.test.tsx
@@ -1,11 +1,57 @@
 import { render, screen } from '@testing-library/react';
+import { PrisionersContext } from '../../context/prisioner-context';
+import { iPrisoner } from '../../interface/prisioner-interface';
 import { InfoPrisioner } from './info-prisioner';
 
 describe('Given the component InfoPrisioner', () => {
+    const mockPrisioners: {
+        prisioners: iPrisoner[];
+    } = {
+        prisioners: [
+            {
+                county_state: '',
+                name: 'Pepe',
+                charges: ['charges'],
+                id: 0,
+                source: 'Maricopa',
+                details: [
+                    ['gender', 'male'],
+                    ['3', 'brown'],
+                    ['yes', 3],
+                ],
+                mugshot: ' string',
+                book_date: '',
+                source_id: '',
+                more_info_url: '',
+            },
+        ],
+    };
     describe('When is called', () => {
-        test('Then it sould print', () => {
-            render(<InfoPrisioner></InfoPrisioner>);
-            const display = screen.getByText(/Personal/i);
+        test('Then it sould details[0]', () => {
+            render(
+                <PrisionersContext.Provider value={mockPrisioners}>
+                    <InfoPrisioner></InfoPrisioner>
+                </PrisionersContext.Provider>
+            );
+            const display = screen.getByText(/male/i);
+            expect(display).toBeInTheDocument();
+        });
+        test('Then it sould print details[1]', () => {
+            render(
+                <PrisionersContext.Provider value={mockPrisioners}>
+                    <InfoPrisioner></InfoPrisioner>
+                </PrisionersContext.Provider>
+            );
+            const display = screen.getByText(/brown/);
+            expect(display).toBeInTheDocument();
+        });
+        test('Then it sould print details[2]', () => {
+            render(
+                <PrisionersContext.Provider value={mockPrisioners}>
+                    <InfoPrisioner></InfoPrisioner>
+                </PrisionersContext.Provider>
+            );
+            const display = screen.getByText(/yes/i);
             expect(display).toBeInTheDocument();
         });
     });
