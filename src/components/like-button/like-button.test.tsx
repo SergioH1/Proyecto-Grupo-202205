5fb55053f8385874a3deb570e89bd98aed284796
Sergio H <sergiohernandezroman@gmail.com>
2022-06-09 18:42:23 +0200

    add texting

--- src/components/like-button/like-button.test.tsx
+++ src/components/like-button/like-button.test.tsx
@@ -1,4 +1,11 @@
+import { User } from '@auth0/auth0-react';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { IndexContext } from '../../context/index-context';
+import { PrisionerLocalContext } from '../../context/local-context';
+import { PrisionersContext } from '../../context/prisioner-context';
+import { iPrisoner } from '../../interface/prisioner-interface';
+import { PrisionerModel } from '../../models/prisioner-local';
 import { LikeButton } from './like-button';
 
 describe('Give the component LikeButton', () => {
@@ -7,6 +14,74 @@ describe('Give the component LikeButton', () => {
             render(<LikeButton></LikeButton>);
             const display = screen.getByAltText(/Icon/i);
             expect(display).toBeInTheDocument();
+        });
+    });
+    describe('when the function is clicked', () => {
+        test('should be call handleMatch', () => {
+            const mockContext: {
+                index: number;
+                forward: () => void;
+                user: User | undefined;
+                isAuthenticated: boolean;
+            } = {
+                index: 1,
+                forward: jest.fn(),
+                user: User,
+                isAuthenticated: false,
+            };
+            const mockContext2: {
+                prisioners: PrisionerModel;
+            } = {
+                prisioners: [
+                    {
+                        name: 'sergio',
+                        id: 8,
+                        image: 'url',
+                        isFav: false,
+                        details: [],
+                    },
+                ],
+                addPrisioner: jest.fn(),
+                deletePrisioner: jest.fn(),
+                updatePrisioner: jest.fn(),
+            };
+
+            const mockContext3: {
+                prisioners: iPrisoner[];
+            } = {
+                prisioners: [
+                    {
+                        county_state: 'Miami',
+                        name: 'Pepe',
+                        charges: ['charges'],
+                        id: 0,
+                        source: 'Maricopa',
+                        details: [
+                            ['s', 0],
+                            ['3', 1],
+                            ['3', 0],
+                        ],
+                        mugshot: ' string',
+                        book_date: '',
+                        source_id: '',
+                        more_info_url: '',
+                    },
+                ],
+            };
+
+            const mockFunction = jest.fn();
+            render(
+                <IndexContext.Provider value={mockContext}>
+                    <PrisionersContext.Provider value={mockContext3}>
+                        <PrisionerLocalContext.Provider value={mockContext2}>
+                            <LikeButton></LikeButton>
+                        </PrisionerLocalContext.Provider>
+                    </PrisionersContext.Provider>
+                </IndexContext.Provider>
+            );
+
+            userEvent.click(screen.getByRole('button'));
+            expect(mockContext.forward).toHaveBeenCalled();
         });
     });
 });
