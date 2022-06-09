f270286034543483ede7656bb8104530bafd9210
Sergio H <sergiohernandezroman@gmail.com>
2022-06-10 00:55:07 +0200

    delete buggs

--- src/pages/favorits.tsx
+++ src/pages/favorits.tsx
@@ -1,11 +1,8 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import { CardList } from '../components/card-favs/card-list';
 import { NavBar } from '../components/navbar/navbar';
 import './favorites.css';
 
 export function Favorits() {
-    const { user } = useAuth0();
-
     return (
         <div className="match-contaienr">
             <div className="head-container">
