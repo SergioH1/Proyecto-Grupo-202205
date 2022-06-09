f065a6b98083b707ac4a01647891e37afb5b984f
Marina <marinadlqs@gmail.com>
2022-06-09 18:11:22 +0200

    Add navbar in favorits page

--- src/pages/favorits.tsx
+++ src/pages/favorits.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { CardList } from '../components/card-favs/card-list';
+import { NavBar } from '../components/navbar/navbar';
 
 export function Favorits() {
     const { user } = useAuth0();
@@ -9,6 +10,7 @@ export function Favorits() {
         <>
             <h2>MATCHES</h2>
             <CardList></CardList>
+            <NavBar></NavBar>
         </>
     );
 }
