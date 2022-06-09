d683ed181b3ce05f0dda0334679cc78c64ee8cf4
Alejandro Montes <ango19941994@gmail.com>
2022-06-09 15:34:29 +0200

    Update tests

--- src/components/DeleteMatch/delete-match.test.tsx
+++ src/components/DeleteMatch/delete-match.test.tsx
@@ -1,12 +1,31 @@
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { click } from '@testing-library/user-event/dist/click';
+import { Simulate } from 'react-dom/test-utils';
+import { PrisionerLocalContext } from '../../context/local-context';
 import { DeleteMatchButton } from './delete-match';
 
 describe('Give the component LikeButton', () => {
     describe('when it is called', () => {
         test('should print picture', () => {
-            render(<DeleteMatchButton></DeleteMatchButton>);
+            render(<DeleteMatchButton id={2}></DeleteMatchButton>);
             const display = screen.getByAltText(/delete/i);
             expect(display).toBeInTheDocument();
+        });
+    });
+    describe('when the button is clicked', () => {
+        test('Then it should execute the mock function', () => {
+            const handleclick = jest.fn();
+            DeleteMatchButton.prototype.handleClick = jest.fn();
+
+            render(
+                <PrisionerLocalContext.Provider>
+                    <DeleteMatchButton id={4}></DeleteMatchButton>
+                </PrisionerLocalContext.Provider>
+            );
+
+            userEvent.click(screen.getByRole('button'));
+            expect(handleclick).toHaveBeenCalled();
         });
     });
 });
--- src/components/info-charge/info-charge.tsx
+++ src/components/info-charge/info-charge.tsx
@@ -1,7 +1,3 @@
-import { useContext } from 'react';
-import { IndexContext } from '../../context/index-context';
-import { PrisionersContext } from '../../context/prisioner-context';
-
 export function InfoCharge({ charges }: { charges: Array<string> }) {
     return (
         <>
