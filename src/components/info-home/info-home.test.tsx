c0f87a4e2fbc97d23f643895813a56d37add4a42
Sergio H <sergiohernandezroman@gmail.com>
2022-06-09 12:55:03 +0200

    add info home testing

--- src/components/info-home/info-home.test.tsx
+++ src/components/info-home/info-home.test.tsx
@@ -1,20 +1,41 @@
 import { screen, render } from '@testing-library/react';
-import { useContext } from 'react';
 import { PrisionersContext } from '../../context/prisioner-context';
-import { PrisionerContextProvider } from '../../context/prisioner-provider';
+import { iPrisoner } from '../../interface/prisioner-interface';
 import { InfoHome } from './info-home';
 
 describe('Given the component InfoHome', () => {
-    const { prisioners } = useContext(PrisionersContext);
-    const prisioner = prisioners[0];
+    const mockPrisioners: {
+        prisioners: iPrisoner[];
+    } = {
+        prisioners: [
+            {
+                county_state: '',
+                name: 'Pepe',
+                charges: ['charges'],
+                id: 0,
+                source: '',
+                details: [
+                    ['s', 0],
+                    ['3', 1],
+                    ['3', 0],
+                ],
+                mugshot: ' string',
+                book_date: '',
+                source_id: '',
+                more_info_url: '',
+            },
+        ],
+    };
+
     describe('when it is called', () => {
         test('should print', () => {
             render(
-                <PrisionerContextProvider>
+                <PrisionersContext.Provider value={mockPrisioners}>
                     <InfoHome></InfoHome>
-                </PrisionerContextProvider>
+                </PrisionersContext.Provider>
             );
-            const display = screen.getByText(prisioner.name);
+
+            const display = screen.getByText(/Pepe/i);
             expect(display).toBeInTheDocument();
         });
     });
